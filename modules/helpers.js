0f098234f730fa699d24f88ffd2e162fef482e5b
DzianisBogdan <thespirit3000@gmail.com>
2020-04-05 20:00:15 +0300

    feat: node helpers add

--- modules/helpers.js
+++ modules/helpers.js
@@ -1,22 +1,21 @@
-export const domArray = (selector) => {
-    return document.querySelectorAll(selector);
-}
+export const domArray = (selector) => document.querySelectorAll(selector);
 
-export const  setUpperCaseClass = (element) => {
-    element.classList.add("text_uppercase");
-}
+export const setUpperCaseClass = (element) => {
+  element.classList.add('text_uppercase');
+};
 
 export const setLowerCaseClass = (element) => {
-    element.classList.remove("text_uppercase");
-}
+  element.classList.remove('text_uppercase');
+};
 
 export const toggleKeyCase = (keyNodeArray, upperSelector) => {
-    keyNodeArray.forEach(element => {
-        if (element.classList.contains(upperSelector)) {
-            element.classList.remove(upperSelector);
-        } else {
-            element.classList.add(upperSelector);
-        }
-    })
-}
-
+  keyNodeArray.forEach((element) => {
+    if (element.classList.contains(upperSelector)) {
+      element.classList.remove(upperSelector);
+    } else {
+      element.classList.add(upperSelector);
+    }
+  });
+};
+export const createElement = (tagName) => document.createElement(tagName);
+export const insertElement = (element, node) => node.appendChild(element);
