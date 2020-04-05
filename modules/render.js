7e518268f0ffbc04718b2c2964cb78134d18f717
DzianisBogdan <thespirit3000@gmail.com>
2020-04-05 20:04:16 +0300

    reafctor: start laout rendering

--- modules/render.js
+++ modules/render.js
@@ -1,25 +1,23 @@
-const wrapperNode = () => {
-    return `
+const wrapperNode = () => `
     <div class="wrapper">
         <h1>codejam-virtual-keyboard</h1>
         <textarea id='input-text' name="text" class='text-area' rows="12">dsgbfhn</textarea>
     </div>
-    `
-}
+    <div id='keyboard' class="wrapper">
+    </div>
+    `;
 
-const footerNode = () => {
-    return `
+const footerNode = () => `
     <div class='width-wrapper'>
         <h5 class='footer'>Клавиатура создана в операционной системе Windows</h5>
         <h5 class='footer'>Для переключения языка комбинация: левыe ctrl + alt</h5>
     </div>
-    `
-}
+    `;
 
 export const renderHTML = () => {
-    document.body.insertAdjacentHTML('afterbegin', wrapperNode())
+  document.body.insertAdjacentHTML('afterbegin', wrapperNode());
 };
 
 export const renderFooter = () => {
-    document.body.insertAdjacentHTML('beforeend', footerNode())
-}
+  document.body.insertAdjacentHTML('beforeend', footerNode());
+};
