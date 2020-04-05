cd0982a63fa3e5b76785772edadbb073ec6d989c
DzianisBogdan <thespirit3000@gmail.com>
2020-04-05 20:03:27 +0300

    feat: keys class

---
+++ modules/NormalKey.js
@@ -0,0 +1,9 @@
+export default class NormalKey {
+  constructor(options) {
+    this.id = options.id;
+    this.ru = options.ru;
+    this.en = options.en;
+    this.width = options.width;
+    this.type = options.type;
+  }
+}
