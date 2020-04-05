5430366d3d2986b64b618f8c2f8ce78bbfac4636
DzianisBogdan <thespirit3000@gmail.com>
2020-04-05 09:36:54 +0300

    feat: linter config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,24 @@
-module.exports = { "extends": "airbnb-base" };
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": [
+        "airbnb-base"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    plugins: ['import'],
+    rules: {
+        'import/extensions': ['error', 'always', {
+            ignorePackages: true,
+            js: 'never',
+        }]
+    }
+};
