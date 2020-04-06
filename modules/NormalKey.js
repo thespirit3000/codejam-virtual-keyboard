d8390839e83b929ca60483822890a2287dede550
DzianisBogdan <thespirit3000@gmail.com>
2020-04-07 01:06:49 +0300

    feat: new properties for class

--- modules/NormalKey.js
+++ modules/NormalKey.js
@@ -3,7 +3,16 @@ export default class NormalKey {
     this.id = options.id;
     this.ru = options.ru;
     this.en = options.en;
+    this.enS = '';
+    this.ruS = '';
     this.width = options.width;
     this.type = options.type;
+
+    if (options.enS) {
+      this.enS = options.enS;
+    }
+    if (options.ruS) {
+      this.ruS = options.ruS;
+    }
   }
 }
